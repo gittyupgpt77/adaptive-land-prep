2ed2b8db5d8351c30face2ba6897e849d3dc3b95
gittyupgpt77 <petralialex@gmail.com>
2026-09-06 21:56:30 -0700

    Refresh cache after calendar and visual fixes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='land-prep-v11';
+const CACHE='land-prep-v12';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
