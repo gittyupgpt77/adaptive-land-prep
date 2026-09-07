3ec3fbe5ccfbfda99025a032d5c4ffdc7a4a3884
gittyupgpt77 <petralialex@gmail.com>
2026-09-06 22:10:50 -0700

    Cache reference exercise photography offline

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='land-prep-v15';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+const CACHE='land-prep-v16';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/back-squat.jpg','./assets/romanian-deadlift.jpg','./assets/walking-lunge.jpg','./assets/rkc-plank.jpg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
