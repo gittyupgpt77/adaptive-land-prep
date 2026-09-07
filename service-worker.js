e1623ca1e8f32bc1bedf8b3b393e7a058e1fb116
gittyupgpt77 <petralialex@gmail.com>
2026-09-06 22:49:59 -0700

    Refresh PWA for real exercise imagery

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='land-prep-v18';
+const CACHE='land-prep-v19';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/back-squat.jpg','./assets/romanian-deadlift.jpg','./assets/walking-lunge.jpg','./assets/rkc-plank.jpg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
